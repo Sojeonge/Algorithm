8b88cb3d30e50fbc6991e94e2cad5b9780b523d4
sojeonge <105636727+Sojeonge@users.noreply.github.com>
2024-10-22 17:42:23 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,6 +1,28 @@
 class Solution {
+    boolean visited[];
+    int[][] data;
+    
     public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
         int answer = 0;
+        data = computers;
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i);
+            }
+        }
         return answer;
+    }
+    
+    void dfs(int node) {
+        visited[node] = true;
+        
+        for(int i = 0; i < data[node].length; i++) {
+            if(!visited[i] && data[node][i] == 1) {
+                dfs(i);
+            }
+        }
     }
 }
