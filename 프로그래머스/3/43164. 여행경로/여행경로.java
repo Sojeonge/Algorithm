5b96c8506b33e1dca0877519016ab51066c19fdd
sojeonge <105636727+Sojeonge@users.noreply.github.com>
2024-10-24 14:56:55 +0900

    Update 여행경로.java

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,6 +1,30 @@
+import java.util.*;
+
 class Solution {
+    static ArrayList<String> list = new ArrayList<>();
+    static boolean[] useTickets;
+    
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        return answer;
+        useTickets = new boolean[tickets.length];
+        
+        dfs (0, "ICN", "ICN", tickets);
+        
+        Collections.sort(list);
+        return list.get(0).split(" ");
+    }
+    
+    static void dfs (int depth, String now, String path, String[][] tickets) {
+        if (depth == tickets.length) {
+            list.add(path);
+            return;
+        }
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (!useTickets[i] && now.equals(tickets[i][0])) {
+                useTickets[i] = true;
+                dfs (depth + 1, tickets[i][1], path + " " + tickets[i][1], tickets);
+                useTickets[i] = false;
+            }
+        }
     }
 }
