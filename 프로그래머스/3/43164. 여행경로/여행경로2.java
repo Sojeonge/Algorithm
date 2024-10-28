c3d435a04d7bed2afeb3613d1e2769f93d4e2e99
sojeonge <105636727+Sojeonge@users.noreply.github.com>
2024-10-28 21:42:24 +0900

    Update 여행경로2.java

--- 프로그래머스/3/43164. 여행경로/여행경로2.java
+++ 프로그래머스/3/43164. 여행경로/여행경로2.java
@@ -9,7 +9,7 @@ class Solution {
         
         dfs (0, "ICN", "ICN", tickets);
         
-        Collections.sort(list);
+        Collections.sort(list); // sort() = 오름차순, reverse() = 내림차순
         return list.get(0).split(" ");
     }
     
