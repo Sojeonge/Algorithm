8de8c59ef9805330620772a198e4bbd517c28058
sojeonge <sojeongdv@gmail.com>
2024-10-31 01:13:02 +0900

    [level 3] Title: 단어 변환, Time: 0.73 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,43 +1,41 @@
 import java.util.*;
 
 class Solution {
+    static int n;
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        boolean[] visited = new boolean[words.length];
+        n = words.length;
+        boolean[] visited = new boolean[n];
         Queue<String> queue = new LinkedList<>();
         
-        queue.add(begin);
+        queue.offer(begin);
         
         while (!queue.isEmpty()) {
-            int size = queue.size();
             
-            for (int s = 0; s < size; s++) {
+            for (int i = 0; i < queue.size(); i++) {
                 String current = queue.poll();
-
-                if (current.equals(target)) {
-                    return answer;
-                }
-
-                for (int i = 0; i < words.length; i++) {
-                    if (!visited[i] && canTrans(current, words[i])) {
-                        visited[i] = true;
-                        queue.add(words[i]);
-                    }
+            
+                if (current.equals(target)) return answer;
+            
+                for (int j = 0; j < n; j++) {
+                    if (!visited[j] && canTransfer(current, words[j])) {
+                        visited[j] = true;
+                        queue.offer(words[j]);
+                    } 
                 }
             }
-            answer++; 
+            answer++;
         }
-        
-        return 0; 
-    }
+        return 0;
+}
     
-static boolean canTrans(String word1, String word2) {
-    int diffCount = 0;
-    for (int i = 0; i < word1.length(); i++) {
-        if (word1.charAt(i) != word2.charAt(i)) {
-            diffCount++;
+public boolean canTransfer(String a, String b) {
+    int count = 0;
+    for (int i = 0; i < a.length(); i++) {
+        if (a.charAt(i) != b.charAt(i)) {
+            count++;
         }
     }
-    return diffCount == 1;
-}        
-}    
+    return count == 1;
+}     
+}
