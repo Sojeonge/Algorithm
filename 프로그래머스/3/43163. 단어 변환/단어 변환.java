5bd08180990929d3d8b42b59790e833d15e244ef
sojeonge <sojeongdv@gmail.com>
2024-10-31 01:12:21 +0900

    [level 3] Title: 단어 변환, Time: 0.73 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,42 +3,41 @@ import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
+        boolean[] visited = new boolean[words.length];
         Queue<String> queue = new LinkedList<>();
-        Set<String> set = new HashSet<>(Arrays.asList(words));
         
-        if (!set.contains(target)) return 0;
-        
-        queue.offer(begin);
+        queue.add(begin);
         
         while (!queue.isEmpty()) {
-            for (int i = 0; i < queue.size(); i++) {
+            int size = queue.size();
+            
+            for (int s = 0; s < size; s++) {
                 String current = queue.poll();
-                
+
                 if (current.equals(target)) {
                     return answer;
                 }
-                
-                for(String word : set.toArray(new String[set.size()])) {
-                    if (canConvert(current, word)) {
-                        queue.offer(word);
-                        set.remove(word);
+
+                for (int i = 0; i < words.length; i++) {
+                    if (!visited[i] && canTrans(current, words[i])) {
+                        visited[i] = true;
+                        queue.add(words[i]);
                     }
                 }
             }
-            
-            answer++;
+            answer++; 
         }
         
-        return 0;
+        return 0; 
     }
-        
-public boolean canConvert(String word1, String word2) {
-    int diffCnt = 0;
+    
+static boolean canTrans(String word1, String word2) {
+    int diffCount = 0;
     for (int i = 0; i < word1.length(); i++) {
         if (word1.charAt(i) != word2.charAt(i)) {
-            diffCnt++;
+            diffCount++;
         }
     }
-    return diffCnt == 1;
-}      
-}
+    return diffCount == 1;
+}        
+}    
