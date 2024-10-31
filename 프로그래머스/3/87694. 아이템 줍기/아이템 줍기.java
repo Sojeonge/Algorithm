277f561e512bfeac4925003227bbfc76801f5a91
sojeonge <sojeongdv@gmail.com>
2024-10-31 15:00:32 +0900

    [level 3] Title: 아이템 줍기, Time: 0.98 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 class Solution {
-    static int[][] map = new int[101][101];
     static int[] dI = {1, -1, 0, 0};
     static int[] dJ = {0, 0, 1, -1};
+    static int[][] map = new int[101][101];
     static boolean[][] visited = new boolean[101][101];
-    static int result = 0;
+    static int answer = 0;
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        drawMap(rectangle); // 테두리(1), 내부(2) 채우기
+        fills(rectangle);
         
-        bfs(characterX * 2, characterY * 2, itemX * 2, itemY * 2);
+        bfs (characterX * 2, characterY * 2, itemX * 2, itemY * 2);
         
-        return result;
+        return answer;     
     }
     
-    static void drawMap(int[][] rectangle) {
+    static void fills(int[][] rectangle) {
         for (int[] rect : rectangle) {
             int startI = rect[1] * 2;
             int endI = rect[3] * 2;
@@ -23,52 +23,46 @@ class Solution {
             int startJ = rect[0] * 2;
             int endJ = rect[2] * 2;
             
-            for(int i=startI;i<=endI;i++)
-            {
-                for(int j=startJ;j<=endJ;j++)
-                {
-                    if(i == startI || i == endI || j == startJ || j == endJ)
-                    {
-                        if(map[i][j] ==2)
-                        {
+            for (int i = startI; i <= endI; i++) {
+                for (int j = startJ; j <= endJ; j++) {
+                    if (i == startI || i == endI || j == startJ || j == endJ) {
+                        if (map[i][j] == 2) {
                             continue;
-                        }
+                        } 
                         map[i][j] = 1;
-                    }
-                    else{
+                    } else {
                         map[i][j] = 2;
                     }
                 }
             }
         }
     }
-    static void bfs(int characterX, int characterY, int itemX, int itemY) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {characterY, characterX, 0});
-        
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
+    
+     static void bfs (int characterX, int characterY, int itemX, int itemY) {
+         Queue<int[]> queue = new LinkedList<>();
+         queue.add(new int[]{characterY, characterX, 0});
+         
+         while (!queue.isEmpty()) {
+            int[] current = queue.poll();
             int currentI = current[0];
             int currentJ = current[1];
-            int currentStep = current[2];
+            int stepCount = current[2];
             
             if (currentI == itemY && currentJ == itemX) {
-                result = currentStep / 2;
-                return;
-            }
+                    answer = stepCount / 2;
+                }
             
             for (int i = 0; i < 4; i++) {
                 int nextI = currentI + dI[i];
                 int nextJ = currentJ + dJ[i];
                 
-                if (nextI >= 1 && nextI <= 100 && nextJ >= 1 && nextJ <= 100) {
+                if (nextI > 0 && nextI < 101 && nextJ > 0 && nextJ < 101) {
                     if (!visited[nextI][nextJ] && map[nextI][nextJ] == 1) {
                         visited[nextI][nextJ] = true;
-                        q.add(new int[]{nextI, nextJ, currentStep + 1});
+                        queue.add(new int[]{nextI, nextJ, stepCount + 1});
                     }
                 }
             }
         }
-    }
-
+     }   
 }
