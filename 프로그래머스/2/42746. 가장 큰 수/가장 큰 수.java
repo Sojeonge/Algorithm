6c59603ab6a500d0066d883e0520086df2be0b9b
sojeonge <sojeongdv@gmail.com>
2024-11-02 00:58:40 +0900

    Update 가장 큰 수.java

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,6 +1,30 @@
+import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        String[] arr = new String[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        
+        // 내림차순 정렬
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+        
+        // Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) return "0"; // 모든 숫자가 0인 경우
+        
+        for (String num : arr) {
+            answer += num;
+        }
+        
         return answer;
     }
 }
