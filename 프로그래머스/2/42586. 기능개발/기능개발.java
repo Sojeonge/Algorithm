5513eef7bfafa1e991b0029f7ee8907573683da1
sojeonge <sojeongdv@gmail.com>
2024-11-05 23:03:00 +0900

    [level 2] Title: 기능개발, Time: 0.30 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,9 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
         Queue<Integer> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
         
         for (int i = 0; i < speeds.length; i++) {
             if ((100 - progresses[i]) % speeds[i] == 0) {
@@ -14,10 +12,15 @@ class Solution {
             }
         }
         
+    //     for (int i = 0; i < speeds.length; i ++) {
+    //         queue.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
+    // }
+        
         int maxDays = queue.poll();
         int count = 1;
+        List<Integer> list = new ArrayList<>();;
         
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             if (maxDays >= queue.peek()) {
                 count++;
                 queue.poll();
@@ -28,14 +31,14 @@ class Solution {
             }
         }
         list.add(count);
-            
-            int[] answer = new int[list.size()];
-            
-            for (int i = 0; i < answer.length; i++) {
-                answer[i] = list.get(i);
-            }
+        
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
         
         return answer;
-          
     }
-}
+    
+} 
