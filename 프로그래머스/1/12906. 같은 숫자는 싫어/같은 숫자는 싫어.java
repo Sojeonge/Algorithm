a4c6d5b5d0619bbf225e8c88c122ebb9e478ac9d
sojeonge <sojeongdv@gmail.com>
2024-11-05 16:30:24 +0900

    Update 같은 숫자는 싫어.java

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,10 +2,21 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
+        int[] answer;
         
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        Stack<Integer> stack = new Stack<>();
+        
+        stack.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (stack.peek() != arr[i]) 
+                stack.push(arr[i]);
+        }
+        
+        answer = new int[stack.size()];
+        
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
 
         return answer;
     }
