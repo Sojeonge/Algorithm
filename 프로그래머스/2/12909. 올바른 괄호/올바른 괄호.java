ba7b98d587fa62383f982734861c7efb85c472b1
sojeonge <sojeongdv@gmail.com>
2024-11-07 21:56:40 +0900

    [level 2] Title: 올바른 괄호, Time: 6.82 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,10 @@
 class Solution {
     boolean solution(String s) {
-        int balance = 0;
+        boolean answer = true;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                balance++;
-            } else {
-                balance--;
-            }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
 
-            // 중간에 balance가 음수가 되면 올바르지 않은 괄호 문자열
-            if (balance < 0) {
-                return false;
-            }
-        }
-
-        // 모든 괄호를 처리하고 balance가 0이면 올바른 문자열
-        return balance == 0;
+        return answer;
     }
 }
