e5da6cc946b850f31f94726b6235290d37c3df1b
sojeonge <sojeongdv@gmail.com>
2024-11-07 21:59:23 +0900

    [level 2] Title: 올바른 괄호, Time: 6.82 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,10 +1,18 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        int balance = 0;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                balance++;
+            } else {
+                balance--;
+            }
 
-        return answer;
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
     }
 }
