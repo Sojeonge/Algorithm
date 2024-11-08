0f5ece6a661f1d253bec6dbcd4be44e0fc170b51
sojeonge <sojeongdv@gmail.com>
2024-11-08 15:52:44 +0900

    Create 프로세스2(리스트).java

---
+++ 프로그래머스/2/42587. 프로세스/프로세스2(리스트).java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        List<int[]> queue = new LinkedList<>();
+        
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new int[]{i, priorities[i]});
+        }
+        
+        while (!queue.isEmpty()) {
+            int[] current = queue.remove(0);
+            boolean hasHigherPriority = false;
+            
+            for (int i = 0; i < queue.size(); i++) {
+                int[] q = queue.get(i);
+                if (q[1] > current[1]) {
+                    queue.add(current);
+                    hasHigherPriority = true;
+                    break;
+                }
+            }
+            
+            if (!hasHigherPriority) {
+                answer++;
+                if (current[0] == location) {
+                    return answer;
+                } 
+            }
+                 
+        }
+         
+        return answer;
+    }
+}
