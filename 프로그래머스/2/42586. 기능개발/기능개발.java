3159a39720a562a6cfb79dee5360990a91daf5bd
sojeonge <sojeongdv@gmail.com>
2024-11-14 23:49:26 +0900

    [level 2] Title: 기능개발, Time: 0.56 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,25 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        int[] days = new int[progresses.length];
         Queue<Integer> queue = new LinkedList<>();
         
-        for (int i = 0; i < speeds.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
-                queue.add((100 - progresses[i]) / speeds[i]);
-            } else {
-                queue.add(((100 - progresses[i]) / speeds[i]) + 1);
-            }
+        for (int i = 0; i < progresses.length; i++) {
+            queue.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
         }
-        
-    //     for (int i = 0; i < speeds.length; i ++) {
-    //         queue.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
-    // }
         
         int maxDays = queue.poll();
         int count = 1;
-        List<Integer> list = new ArrayList<>();;
+        List<Integer> list = new ArrayList<>();
         
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
+            
             if (maxDays >= queue.peek()) {
                 count++;
                 queue.poll();
@@ -28,17 +22,17 @@ class Solution {
                 list.add(count);
                 count = 1;
                 maxDays = queue.poll();
-            }
+            }            
         }
+        
         list.add(count);
         
         int[] answer = new int[list.size()];
         
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
-        }
+        }   
         
-        return answer;
+        return answer;      
     }
-    
-} 
+}
