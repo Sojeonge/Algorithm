3875d708987a218b0dc2dc2a07e3257189ae7447
SOJEONG, LEE <sojeongdv@gmail.com>
2024-12-10 00:42:14 +0900

    Update 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -11,3 +11,13 @@ AND CAR_ID IN (SELECT CAR_ID
 GROUP BY CAR_ID, MONTH
 HAVING RECORDS > 0
 ORDER BY MONTH, CAR_ID DESC;
+
+
+// %Y : 년도(2024) / %y : 년도(21)
+// %m : 월(01,02,11) / %c : 월(1,8) / %M : 월(January, August) / %b : 월(Jan, Aug)
+// %d : 일(01,19) / %e : 일(1,19)
+// %W : 요일(Wednesday, friday) / %a : 요일(Wed, Fri)
+// %T : 시간 (12:30:00) / %r : 시간 (12:30:00 AM)
+// %H : 24시간 시간(01, 14, 18) / %l : 12시간 시간 (01, 02, 06)
+// %i : 분 (00)
+// %S : 초 (00)
