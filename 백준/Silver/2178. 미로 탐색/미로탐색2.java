5ce5d7845d2fe2f2217fdbaa9b15ad95e17e21ae
SOJEONG, LEE <sojeongdv@gmail.com>
2024-12-18 23:16:29 +0900

    Update 미로탐색2.java

--- 백준/Silver/2178. 미로 탐색/미로탐색2.java
+++ 백준/Silver/2178. 미로 탐색/미로탐색2.java
@@ -2,52 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static int[][] map;
   static int[] dx = {1, -1, 0, 0};
-  static int[] dy = {0, 0, -1, 1};
+  static int[] dy = {0, 0, 1, -1};
+  static int n;
+  static int[][] map;
+
+  static int count;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    
-    // String[] str = br.readLine().split(" ");
-    // int n = Integer.parseInt(str[0]);
-    // int m = Integer.parseInt(str[1]);
+    n = Integer.parseInt(br.readLine());
+    map = new int[n][n];
+    List<Integer> list = new ArrayList<>();
 
-    map = new int[n][m];
     for (int i = 0; i < n; i++) {
-      String input = br.readLine();
-      for (int j = 0; j < m; j++) {
-        map[i][j] = input.charAt(j) -'0';
+      String s = br.readLine();
+      for (int j = 0; j < n; j++) {
+        map[i][j] = s.charAt(j) - '0';
       }
     }
 
-    boolean visited[][] = new boolean[n][m];
-    Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[] {0, 0});
-    visited[0][0] = true;
-
-    while (!queue.isEmpty()) {
-      int[] now = queue.poll();
-      int nowX = now[0];
-      int nowY = now[1];
-
-      for (int i = 0; i < 4; i++) {
-        int nx = nowX + dx[i];
-        int ny = nowY + dy[i];
-
-        if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] || map[nx][ny] == 0)
-          continue;
-
-        visited[nx][ny] = true;
-        queue.add(new int[] {nx, ny});
-        map[nx][ny] = map[nowX][nowY] + 1; // 최단거리로 카운팅
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        count = 0;
+        if (map[i][j] == 1) {
+          count++;
+          bfs(i,j);
+          list.add(count);
+        }
       }
     }
+    System.out.println(list.size());
+    Collections.sort(list);
+    for (int result : list) {
+      System.out.println(result);
+    }
+  }
 
-    System.out.println(map[n-1][m-1]);
+  private static void bfs(int i, int j) {
+    Queue<int[]> q = new LinkedList<>();
+    q.add(new int[]{i,j});
+    map[i][j] = 0;
+
+    while(!q.isEmpty()) {
+      int[] cur = q.poll();
+      int x = cur[0];
+      int y = cur[1];
+
+      for (int k = 0; k < 4; k++) {
+        int nx = x + dx[k];
+        int ny = y + dy[k];
+
+        if (nx >= 0 && ny >= 0 && nx < n && ny < n && map[nx][ny] == 1) {
+          count++;
+          map[nx][ny] = 0;
+          q.add(new int[]{nx, ny});
+        }
+      }
+    }
   }
 
 }
