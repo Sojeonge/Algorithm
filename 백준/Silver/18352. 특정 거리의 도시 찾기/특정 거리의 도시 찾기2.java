7e1a67f4adab5c141eb73c4565bee27636ebd79f
SOJEONG, LEE <sojeongdv@gmail.com>
2024-12-31 23:01:20 +0900

    Update 특정 거리의 도시 찾기2.java

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기2.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기2.java
@@ -1,1 +1,50 @@
+import java.util.*;
 
+public class Main {
+    static int n;
+    static int[] table;
+    static ArrayList<ArrayList<Integer>> graph;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int x = sc.nextInt();
+        table = new int[n+1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        Arrays.fill(table, (int)1e9);
+        for (int i = 0; i < m; i++) {
+            graph.get(sc.nextInt()).add(sc.nextInt());
+        }
+        table[x] = 0;
+
+        dijkstra(x);
+        int cnt = 0;
+        for (int i = 1; i < table.length; i++) {
+            if (table[i] == k) {
+                System.out.println(i);
+                cnt++;
+            }
+        }
+        if (cnt == 0) System.out.println(-1);
+    }
+
+    static void dijkstra(int start) {
+        PriorityQueue<Integer> que = new PriorityQueue<>();
+        que.offer(start);
+        while (!que.isEmpty()) {
+            int now = que.poll();
+            for (int j = 0; j < graph.get(now).size(); j++) {
+                int cost = table[now] + 1;
+                if (cost < table[graph.get(now).get(j)]) {
+                    table[graph.get(now).get(j)] = cost;
+                    que.offer(graph.get(now).get(j));
+                }
+            }
+        }
+    }
+
+}
