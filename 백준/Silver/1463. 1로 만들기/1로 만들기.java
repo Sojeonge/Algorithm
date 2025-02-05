1556a29c8c88db89ba25969278fef518eb462f38
SOJEONG, LEE <sojeongdv@gmail.com>
2025-02-05 23:55:00 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 18072 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+  static int N;
+  static int count;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+
+    System.out.println(bfs(N));
+
+  }
+
+  private static int bfs(int N) {
+    Queue<Integer> q = new LinkedList<>();
+    int[] visited = new int[N + 1];
+
+    q.add(N);
+    visited[N] = 1;
+
+    while (!q.isEmpty()) {
+      int cur = q.poll();
+
+      if (cur == 1) {
+        return visited[cur] - 1;
+      }
+
+      if (cur % 3 == 0 && visited[cur / 3]== 0) {
+        visited[cur / 3] = visited[cur] + 1;
+        q.add(cur / 3);
+      }
+
+      if (cur % 2 == 0 && visited[cur / 2] == 0) {
+        visited[cur / 2] = visited[cur] + 1;
+        q.add(cur / 2);
+      }
+
+      if (visited[cur - 1] == 0) {
+        visited[cur - 1] = visited[cur] + 1;
+        q.add(cur - 1);
+      }
+    }
+    return 0;
+  }
+}
