14a6afb3c07ea617957344703eb14f8b4e7e8d05
SOJEONG, LEE <sojeongdv@gmail.com>
2025-02-06 23:41:22 +0900

    Update 1로 만들기(DP).java

--- 백준/Silver/1463. 1로 만들기/1로 만들기(DP).java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기(DP).java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // DP 테이블 (각 숫자를 1로 만드는 데 필요한 최소 연산 횟수)
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            // 1을 빼는 경우
+            dp[i] = dp[i - 1] + 1;
+            // 2로 나누어 떨어지는 경우
+            if(i % 2 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            // 3으로 나누어 떨어지는 경우
+            if(i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        System.out.println(dp[n]);
+    }
+}
