c928bbc990e2d91e49d99527ba67a3b7c43b0f36
SOJEONG, LEE <sojeongdv@gmail.com>
2025-02-10 22:15:33 +0900

    Update 1， 2， 3 더하기.java

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int t = Integer.parseInt(br.readLine());
 		int[] dp = new int[11];
 		dp[1] = 1;
@@ -14,9 +15,11 @@ public class Main {
 		for(int i=4; i<11; i++) {
 			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
 		}
+		
 		for(int i=0; i<t; i++) {
 			int num = Integer.parseInt(br.readLine());
-			System.out.println(dp[num]);
+			sb.append(dp[num] + "\n");
 		}
+		System.out.println(sb);
 	}
 }
