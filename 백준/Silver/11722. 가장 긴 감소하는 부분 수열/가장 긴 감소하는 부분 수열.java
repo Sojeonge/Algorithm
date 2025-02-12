3e386e3845a772bb5c8683e384403e0df05def19
SOJEONG, LEE <sojeongdv@gmail.com>
2025-02-12 22:47:31 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 120 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,30 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class Main{
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.valueOf(br.readLine());
-		String [] input = br.readLine().split(" ");
-		int [] array = new int [N+1];
-		int [] dp = new int [N+1];
-		for(int i=1;i<array.length;i++) {
-			array[i]=Integer.valueOf(input[i-1]);
-			dp[i]=1;
-		}
-//		System.out.println(Arrays.toString(array));
-		
-		for(int i=1;i<dp.length;i++) {
-			for(int j=1;j<i;j++) {
-				if(array[i]<array[j] && dp[i]<dp[j]+1) {
-					dp[i]=dp[j]+1;
-				}
-			}
-		}
-//		System.out.println(Arrays.toString(dp));
-		Arrays.sort(dp);
-		System.out.println(dp[N]);
-	}
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    String[] str = br.readLine().split(" ");
+    int[] arr = new int[n + 1];
+    int[] dp = new int[n + 1];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(str[i]);
+      dp[i] = 1;
+    }
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        if (arr[i] < arr[j]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+      }
+    }
+    int result = 0;
+    for (int i = 0; i < dp.length; i++) {
+      result = Math.max(result, dp[i]);
+    }
+
+    System.out.println(result);
+  }
 }
