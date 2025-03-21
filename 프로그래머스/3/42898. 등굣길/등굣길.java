65c522dd61a0bc8008cf638bd18d841c3973ff34
SOJEONG, LEE <sojeongdv@gmail.com>
2025-03-21 23:26:29 +0900

    [level 3] Title: 등굣길, Time: 1.14 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int mod = 1000000007;
+        int[][] dp = new int[n][m];
+        
+        for (int[] puddle : puddles) {
+            dp[puddle[1] - 1][puddle[0] - 1] = -1;
+        }
+        
+        dp[0][0] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dp[i][j] == -1) continue;
+                
+                if (i > 0 && dp[i - 1][j] > 0) {
+                    dp[i][j] += dp[i - 1][j]; // 위에서 
+                }
+                
+                if (j > 0 && dp[i][j - 1] > 0) {
+                    dp[i][j] += dp[i][j - 1]; // 왼쪽에서
+                }
+                
+                dp[i][j] %= mod;
+            }
+        }
+        
+        return dp[n - 1][m - 1];
+    }
+}
