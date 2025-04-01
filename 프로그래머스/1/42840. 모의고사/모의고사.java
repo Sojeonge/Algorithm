5faa54a52ae59a8575cf1a47b2d0d0b1e03242c3
SOJEONG, LEE <sojeongdv@gmail.com>
2025-04-02 00:12:41 +0900

    [level 1] Title: 모의고사, Time: 4.40 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int[][] stu = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        int[] count = new int[stu.length];
+        
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < stu.length; j++) {
+                if (answers[i] == stu[j][(i % stu[j].length)]) {
+                    count[j] += 1;
+                }
+            }
+        }
+        
+        int max = Math.max(count[0],Math.max(count[1], count[2]));
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < count.length; i++) {
+            if (max == count[i]) {
+                list.add(i + 1);
+            }
+        }
+    
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
