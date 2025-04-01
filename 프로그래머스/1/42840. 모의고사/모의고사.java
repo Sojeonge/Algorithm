af50f76ebb1c1c195b0632825b4923f1ad82cd9e
SOJEONG, LEE <sojeongdv@gmail.com>
2025-04-02 00:16:21 +0900

    Update 모의고사.java

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -28,5 +28,12 @@ class Solution {
         }
     
         return list.stream().mapToInt(i->i).toArray();
+
+        // int[] result = new int[list.size()];
+        // for (int i = 0; i < list.size(); i++) {
+        //     result[i] = list.get(i);
+        // }
+    
+        // return result;
     }
 }
