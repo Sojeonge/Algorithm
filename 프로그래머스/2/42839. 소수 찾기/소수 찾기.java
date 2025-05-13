0dda1ded2445d54fe29f542d499340a3569afe53
SOJEONG, LEE <sojeongdv@gmail.com>
2025-05-13 17:34:39 +0900

    [level 2] Title: 소수 찾기, Time: 36.30 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,50 @@
-import java.io.*;
 import java.util.*;
+
 class Solution {
     static ArrayList<Integer> arr = new ArrayList<>();
-    static boolean[] check = new boolean[7];
+    static boolean[] check;
     
     public int solution(String numbers) {
         int answer = 0;
-        for(int i=0; i<numbers.length(); i++){
-            dfs(numbers,"",i+1);
+        check = new boolean[7];
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            dfs(numbers, "", i + 1);
         }
         
-        for(int i=0; i<arr.size(); i++){
-            if(prime(arr.get(i))) answer++;              
+        for (int i = 0; i < arr.size(); i++) {
+            if (prime(arr.get(i))) answer++;
         }
-        
+            
         return answer;
-  
     }
-	//백트래킹
-	static void dfs(String str, String temp, int m) {
-            if(temp.length() == m){
-                int num = Integer.parseInt(temp);
-                if(!arr.contains(num)){
-                    arr.add(num);
-                }
-            }
+    
+    static void dfs(String str, String temp, int count) {
+        if (temp.length() == count) {
+            int num = Integer.parseInt(temp);
         
-            for(int i=0; i<str.length(); i++){
-                if(!check[i]){
-                    check[i] = true;
-                    temp += str.charAt(i);
-                    dfs(str, temp, m);
-                    check[i] = false;
-                    temp = temp.substring(0, temp.length()-1);
-                }
+            if (!arr.contains(num)) {
+                arr.add(num);
             }
-		
-	}
-	//소수 판단
-	static boolean prime(int n) {
-		if(n<2) return false;
-		
-		for(int i=2; i*i<=n; i++) {
-			if(n % i == 0) return false;
-		}
-		
-		return true;
-	}
-
+        }
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (!check[i]) {
+                check[i] = true;
+                dfs(str, temp + str.charAt(i), count);
+                check[i] = false;
+            }
+        }
+    }
+    
+    
+    static boolean prime(int num) {
+        if (num < 2) return false;
+        
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
+        }
+        
+        return true;
+    }    
 }
