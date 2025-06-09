074296e0a0eb0e05c56c52d4364deabcfcacb8e3
SOJEONG, LEE <sojeongdv@gmail.com>
2025-06-09 13:26:35 +0900

    Update 문자열 교환(슬라이딩 윈도우).java

--- 백준/Silver/1522. 문자열 교환/문자열 교환(슬라이딩 윈도우).java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환(슬라이딩 윈도우).java
@@ -1,1 +1,32 @@
+import java.io.*;
 
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] str = br.readLine().split("");
+
+    int aCnt = 0;
+    for (int i = 0; i < str.length; i++) {
+      if (str[i].equals("a")) aCnt++;
+    }
+
+    int start = 0;
+    int end = aCnt - 1;
+
+    int bCnt = 0;
+    for (int i = 0; i < aCnt; i++) {
+      if (str[i].equals("b")) bCnt++;
+    }
+
+    int min = bCnt;
+    while (start < str.length) { // 슬라이딩 윈도우
+      if (str[++end % str.length].equals("b")) bCnt++;
+      if (str[start++].equals("b")) bCnt--;
+
+      min = Math.min(min, bCnt);
+    }
+
+    System.out.println(min);
+  }
+}
